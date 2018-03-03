dbbb04cc5f8a2352e6bdba64e4e64ff8c1809b1c
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-03 01:31:10 -0800

    Adding a bit more content

---
+++ assets/js/feature.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function (){
+    Typed.new('.element',{
+        strings: ["Tech Enthusiast", "PetrolHead", "AI Explorer"],
+        typeSpeed: 20
+    });
+});
