ee745baf17bffa3953a9c1f0468137cb1a1b94ab
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-03 02:52:13 -0800

    Similar commit

--- assets/js/feature.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function (){
-    Typed.new('.element',{
-        strings: ["Tech Enthusiast", "PetrolHead", "AI Explorer"],
-        typeSpeed: 20
-    });
-});
